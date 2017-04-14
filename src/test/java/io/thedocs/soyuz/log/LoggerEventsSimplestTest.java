065eb3e741ef4d8cb6b9bc03865ec700dab0ae2f
fbelov <fbelov@gramant.ru>
2017-04-14 17:37:44 +0300

    tests

---
+++ src/test/java/io/thedocs/soyuz/log/LoggerEventsSimplestTest.java
@@ -0,0 +1,18 @@
+package io.thedocs.soyuz.log;
+
+import io.belov.soyuz.utils.to;
+import org.junit.Test;
+
+/**
+ * Created on 14.04.17.
+ */
+public class LoggerEventsSimplestTest {
+
+    private static final LoggerEvents loge = LoggerEvents.getInstance(LoggerEventsSimplestTest.class);
+
+    @Test
+    public void main() {
+        loge.info("world.hello", to.map("project", "soyuz-loge")); //world.hello: {project=soyuz-loge}
+    }
+
+}
---
+++ src/test/java/io/thedocs/soyuz/log/LoggerEventsTest.java
@@ -0,0 +1,60 @@
+package io.thedocs.soyuz.log;
+
+import io.belov.soyuz.utils.to;
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created on 14.04.17.
+ */
+public class LoggerEventsTest {
+
+    private static final LoggerEvents loge = LoggerEvents.getInstance(LoggerEventsTest.class);
+
+    @Test
+    public void shouldCorrectlyLogEvents() {
+        //when
+        loge.warn("user.register.e.notUnique", to.map("mail", "hello@gmail.com"));
+        loge.debug("user.login", to.map("mail", "hello@gmail.com"));
+        loge.info("task.created", to.map("user", "hello@gmail.com", "label", "Improve documentation", "type", "todo"));
+        loge.error("task.process.e", to.map("id", 123, "type", "todo"), new IllegalStateException());
+
+        //then
+        assertLogs(to.list(
+                "WARN  i.thedocs.soyuz.log.LoggerEventsTest - user.register.e.notUnique: {mail=hello@gmail.com}",
+                "DEBUG i.thedocs.soyuz.log.LoggerEventsTest - user.login: {mail=hello@gmail.com}",
+                "INFO  i.thedocs.soyuz.log.LoggerEventsTest - task.created: {label=Improve documentation, type=todo, user=hello@gmail.com}",
+                "ERROR i.thedocs.soyuz.log.LoggerEventsTest - task.process.e: {id=123, type=todo}"
+        ));
+    }
+
+    private void assertLogs(List<String> logsExpected) {
+        List<String> logs = getLogContent();
+
+        for (String logExpected : logsExpected) {
+            assertContainLogEntry(logs, logExpected);
+        }
+    }
+
+    private void assertContainLogEntry(List<String> logs, String logExpected) {
+        for (String log : logs) {
+            if (log.contains(logExpected)) return;
+        }
+
+        fail("Logs do not contain " + logExpected);
+    }
+
+    private List<String> getLogContent() {
+        try {
+            return FileUtils.readLines(new File(System.getProperty("java.io.tmpdir"), "loge-test.log"), "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
